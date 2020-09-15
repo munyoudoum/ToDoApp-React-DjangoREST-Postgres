1578697c5da0d31e4650fb412e78ee488a890332
munyoudoum <pav.munyoudoum19@kit.edu.kh>
2020-09-16 02:21:22 +0700

    change static url

--- todoro/settings.py
+++ todoro/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'build/static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "build/static")]
 
 CORS_ALLOWED_ORIGINS = [
