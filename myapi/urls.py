608642aaa72491ea2dec966dfba0bd2f7b255869
munyoudoum <pav.munyoudoum19@kit.edu.kh>
2020-09-15 17:23:06 +0700

    Initial Commit

---
+++ myapi/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', views.apiOverview, name="api-overview"),
+    path('task-list/', views.taskList, name="task-list"),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+    path('task-create/', views.taskCreate, name="task-craete"),
+    path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+    path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
+]
